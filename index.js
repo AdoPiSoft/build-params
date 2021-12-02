d24991c25e36435e9c7d6975b98042b0ae524b6b
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2021-12-02 13:52:33 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+  const { NODE_ENV, DROP_CONSOLE, DEBUG, prod, LITE, lite } = process.env
+  const _lite = LITE || lite
+  const _debug = DEBUG || process.env.debug
+  const _drop_console = DROP_CONSOLE || process.env.drop_console
+  const production = NODE_ENV === 'production' || prod === '1' || prod === 'true' || prod === 'yes'
+  const debug = _debug === '1' || _debug === 'true' || _debug === 'yes'
+  const drop_console = _drop_console === '1' || _drop_console === 'true' || _drop_console === 'yes'
+  const is_lite = _lite === '1' || _lite === 'yes' || _lite === 'true'
+  const dev = !production && !debug
+
+  return {
+    prod: true,
+    dev,
+    production,
+    debug,
+    drop_console,
+    lite: is_lite
+  }
+}
